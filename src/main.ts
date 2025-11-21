bf5c7d8960114ca11b288be5a8135a1e19452f72
Eyüp <epsilonelittr@gmail.com>
2025-11-21 11:29:58 +0300

    Update dependencies to latest versions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import './style.scss';
+import './style.css';
 import App from './App.vue';
 import router from './router';
 import Vue3ColorPicker from 'vue3-colorpicker';
